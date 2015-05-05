8a4f6783523e6376e993fe795873f54e65de1684
Yaroslav Ribyak <y4@miritec.com>
2015-05-05 11:04:06 +0300

    header fix

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 session_start();
 if (!empty($_SESSION["data"])) {
     header('Location: /application/view/view_profile.php', true, 303);
+    exit;
 }
 else {
-    header('Location: /application/view/view_login.php', true, 300);
+    header('Location: /application/view/view_login.php', true, 303);
+    exit;
 }
