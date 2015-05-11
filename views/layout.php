4f4080922b5d8f80dfa7bb09e8ce323fcbca4422
Yaroslav Rybyak <yaroslav.rybyak@ya.ru>
2015-05-11 12:47:36 +0300

    CSS

--- views/layout.php
+++ views/layout.php
@@ -1,6 +1,12 @@
 <DOCTYPE html>
-    <html>
+    <html lang="ru">
     <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
+        <link href="/css/bootstrap.min.css" rel="stylesheet">
+        <link href="/css/sticky-footer.css" rel="stylesheet">
     </head>
     <body>
     <header>
@@ -8,5 +14,10 @@
         <a href='?controller=posts&action=index'>Posts</a>
     </header>
     <?php require_once('routes.php'); ?>
-    <body>
-    <html>
+    <footer class="footer">
+        <div class="container">
+            <p class="text-muted">Yaroslav Rybyak 2015</p>
+        </div>
+    </footer>
+    </body>
+</html>
