c96811f4bd3433f66291aa0bb175554c040d3d57
Yaroslav Rybyak <yaroslav.rybyak@ya.ru>
2015-05-11 13:25:09 +0300

    footer

--- views/layout.php
+++ views/layout.php
@@ -27,7 +27,7 @@
 </div>
 <footer class="footer">
     <div class="container">
-        <p class="text-muted">Yaroslav Rybyak 2015</p>
+        <p class="text-muted"><a href="https://github.com/y-rybyak" target="_blank">Yaroslav Rybyak</a> <?= date('Y') ?></p>
     </div>
 </footer>
 </body>
