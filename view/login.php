e24583e962a12043e17b731a1506bbf66730f737
Yaroslav Ribyak <y4@miritec.com>
2015-05-13 18:59:35 +0300

    refactoring

--- view/login.php
+++ view/login.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="/css/bootstrap.css" rel="stylesheet">
-    <title><?= $result ?></title>
+    <title>Login</title>
 </head>
 <body>
 <nav class="navbar navbar-default">
